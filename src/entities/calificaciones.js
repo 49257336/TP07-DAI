126acc2bfb3ce780cacf77db684b0e37704a1d80
49257336 <49257336@est.ort.edu.ar>
2026-06-08 10:33:46 -0300

    Add files via upload

---
+++ src/entities/calificaciones.js
@@ -0,0 +1,8 @@
+export default class Calificacion {
+    constructor(nota, id_alumno , id_materia, fecha ) {
+        this.nota             = nota;
+        this.id_alumno        = id_alumno;
+        this.id_materia       = id_materia;
+        this.fecha            = fecha;
+    }
+}
